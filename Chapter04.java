a73de3ae076b462d83ef67c306d3982904b259a8
EC2 Default User <k9batai33@gmail.com>
2022-09-03 06:20:47 +0000

    変数について

---
+++ Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04 {
+  public static void main(String[] args) {
+    String name = "小林太一";
+    System.out.println(name);
+    
+    name = "バナナ";
+    System.out.println(name);
+    
+    final int NUM = 3;
+    System.out.println(NUM);
+  }
+}
